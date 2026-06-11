09399661f5e12ffd7508b2e737a4f11507911540
Jongha611 <kimjongha611@gmail.com>
2026-06-11 23:31:20 +0900

    apply gradient accumulation

--- linear_head/train.py
+++ linear_head/train.py
@@ -15,6 +15,7 @@ def train_model(
     optimizer: torch.optim.Optimizer,
     epoch: int,
     num_epochs: int = 1,
+    accumulation_steps: int = 1,
 ) -> tuple[float, float]:
     model.train()
 
@@ -25,13 +26,13 @@ def train_model(
     avg_loss = 0.0
     train_acc = 0.0
 
+    optimizer.zero_grad()
+
     # learning loop
     progress_bar = tqdm(
         iterable=train_loader, desc=f"Epoch {epoch+1}/{num_epochs} [Train]"
     )
     for index, batch in enumerate(progress_bar):
-        optimizer.zero_grad()
-
         images, labels = batch
         images = images.to(device)
         labels = labels.to(device)
@@ -40,11 +41,19 @@ def train_model(
             logits = model(images)
             loss = criterion(logits, labels)
 
-        loss.backward()
-        optimizer.step()
+        # gradient accumulation을 위해 스케일링 전의 loss 값을 보존합니다.
+        loss_item = loss.item()
+
+        # 스텝수만큼 손실을 나누고 역전파합니다.
+        (loss / accumulation_steps).backward()
+
+        # accumulation_steps만큼 스텝을 쌓거나, 마지막 스텝인 경우 optimizer를 1회 업데이트합니다.
+        if (index + 1) % accumulation_steps == 0 or (index + 1) == len(train_loader):
+            optimizer.step()
+            optimizer.zero_grad()
 
         # loss
-        running_loss += loss.item()
+        running_loss += loss_item
         avg_loss = running_loss / (index + 1)
 
         # accuracy
